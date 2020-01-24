4f2f312cadabaae98cd7b4c5f365d6b826274042
THolovacko <tom.holovacko@gmail.com>
2020-01-24 10:35:31 -0500

    rendering stuff

--- multiplayer_game_2d/main.cpp
+++ multiplayer_game_2d/main.cpp
@@ -1,22 +1,37 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include <windows.h>
 
 int main()
 {
   // create window
-  sf::VideoMode highest_resolution_video_mode;
-  {
-    std::vector<sf::VideoMode> full_screen_modes = sf::VideoMode::getFullscreenModes();
-    if (full_screen_modes.size() == 0)
-    {
-      std::cout << "no full screen modes returned from sf::VideoMode::getFullscreenModes()";
-      return 0;
-    }
-    highest_resolution_video_mode = full_screen_modes[0];
-  }
-  sf::Window window(highest_resolution_video_mode, "2D Multiplayer Game", sf::Style::Fullscreen);
+  sf::VideoMode desktop_video_mode = sf::VideoMode::getDesktopMode();
+  sf::RenderWindow window(desktop_video_mode, "2D Multiplayer Game", sf::Style::Fullscreen);
   window.setVerticalSyncEnabled(true);
+  window.setActive(true);
+  sf::Vector2u window_size = window.getSize();
+
+
+
+  sf::Texture background_texture;
+  sf::Sprite  background;
+  background_texture.loadFromFile("Assets/Images/bomberman_map.png");
+	sf::Vector2u background_texture_size = background_texture.getSize();
+  float background_scale_x = (float) window_size.x / background_texture_size.x;
+  float background_scale_y = (float) window_size.y / background_texture_size.y;
+  background.setTexture(background_texture);
+	background.setScale(background_scale_x, background_scale_y);
+
+  sf::Texture local_player_texture;
+  sf::Sprite  local_player;
+  local_player_texture.loadFromFile("Assets/Images/mario.png");
+  local_player_texture.setSmooth(true);
+	sf::Vector2u local_player_texture_size = local_player_texture.getSize();
+  float local_player_scale_x = (float) (window_size.x / 16.0f) / local_player_texture_size.x;
+  float local_player_scale_y = (float) (window_size.y / 16.0f) / local_player_texture_size.y;
+  local_player.setTexture(local_player_texture);
+	local_player.setScale(local_player_scale_x, local_player_scale_y);
+
 
 
   // setup and run game loop
@@ -33,12 +48,17 @@ int main()
     elapsed_frame_time_milliseconds = elapsed_frame_time.asMilliseconds();
     elapsed_frame_time_microseconds = elapsed_frame_time.asMicroseconds();
     elapsed_frame_time_seconds      = elapsed_frame_time.asSeconds();
-    std::cout << elapsed_frame_time_seconds << std::endl;
+    std::cout << "elapsed_frame_time_milliseconds: " << elapsed_frame_time_milliseconds << std::endl;
 
     while (window.pollEvent(window_event))
     {
       if (window_event.type == sf::Event::Closed) window.close();
     }
+
+    window.clear(sf::Color::Black);
+    window.draw(background);
+    window.draw(local_player);
+    window.display();
 
     if (elapsed_frame_time_seconds == 0.0f) Sleep(1); // handle underflow
   } // end of game loop
