c8986036c19f9961d293f1e4cb856b450e072934
THolovacko <tom.holovacko@gmail.com>
2020-01-25 08:28:03 -0500

    framerate independent movement

--- multiplayer_game_2d/main.cpp
+++ multiplayer_game_2d/main.cpp
@@ -25,12 +25,20 @@ int main()
   sf::Texture local_player_texture;
   sf::Sprite  local_player;
   local_player_texture.loadFromFile("Assets/Images/mario.png");
-  local_player_texture.setSmooth(true);
   sf::Vector2u local_player_texture_size = local_player_texture.getSize();
   float local_player_scale_x = (float) (window_size.x / 16.0f) / local_player_texture_size.x;
   float local_player_scale_y = (float) (window_size.y / 16.0f) / local_player_texture_size.y;
   local_player.setTexture(local_player_texture);
   local_player.setScale(local_player_scale_x, local_player_scale_y);
+
+  sf::Texture bomb_texture;
+  sf::Sprite  bomb;
+  bomb_texture.loadFromFile("Assets/Images/bomb.png");
+  sf::Vector2u bomb_texture_size = bomb_texture.getSize();
+  float bomb_scale_x = (float) (window_size.x / 16.0f) / bomb_texture_size.x;
+  float bomb_scale_y = (float) (window_size.y / 16.0f) / bomb_texture_size.y;
+  bomb.setTexture(bomb_texture);
+  bomb.setScale(bomb_scale_x, bomb_scale_y);
 
 
 
@@ -44,23 +52,38 @@ int main()
 
   while (window.isOpen())
   {
+    // determine framerate
     elapsed_frame_time = clock.restart();
     elapsed_frame_time_milliseconds = elapsed_frame_time.asMilliseconds();
     elapsed_frame_time_microseconds = elapsed_frame_time.asMicroseconds();
     elapsed_frame_time_seconds      = elapsed_frame_time.asSeconds();
     std::cout << "elapsed_frame_time_milliseconds: " << elapsed_frame_time_milliseconds << std::endl;
 
+
+
+    // get input and events
     while (window.pollEvent(window_event))
     {
       if (window_event.type == sf::Event::Closed) window.close();
     }
 
+
+
+    // calculate gameplay stuff
+    bomb.move(0.0f,100.0f * elapsed_frame_time_seconds);
+
+
+
+    // draw
     window.clear(sf::Color::Black);
     window.draw(background);
     window.draw(local_player);
+    window.draw(bomb);
     window.display();
 
-    if (elapsed_frame_time_seconds == 0.0f) Sleep(1); // handle underflow
+
+    // handle underflow
+    if (elapsed_frame_time_seconds == 0.0f) Sleep(1);
   } // end of game loop
 
   return 0;
